07926a49a8cda2c212f5831eba81719efd0a67c6
PallaviKhedle <pallavi8khedle@gmail.com>
2024-04-23 22:33:43 -0700

    first servlet project

---
+++ welcomeServlet/src/main/java/com/welcome/HelloServlet.java
@@ -0,0 +1,43 @@
+package com.welcome;
+
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+/**
+ * Servlet implementation class HelloServlet
+ */
+public class HelloServlet extends HttpServlet {
+	
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+//		response.getWriter().append("Served at: ").append(request.getContextPath());
+		    response.setContentType("text/html"); 
+         
+	        PrintWriter writer = response.getWriter(); 
+	          
+	        writer.append("<html>"); 
+	        writer.append("<body>"); 
+	        writer.append("<h1>Welcome Servlet</h1>"); 
+	        writer.append("</html>"); 
+	        writer.append("</body>"); 
+	      
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+//	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+//		// TODO Auto-generated method stub
+//		doGet(request, response);
+//	}
+
+}
